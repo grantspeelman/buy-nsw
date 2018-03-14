06bcfce2944cdc29d38f5ada7317143a9e714684
Jordan Hatch <jordan@jordanh.net>
2018-03-15 10:59:27 +1100

    Add basic username and password auth to the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate!
 
 private
+  def authenticate!
+    return unless ENV['BASIC_USERNAME'].present? && ENV['BASIC_PASSWORD'].present?
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_USERNAME'] && password == ENV['BASIC_PASSWORD']
+    end
+  end
+
   def error_404
     render file: Rails.root.join('public', '404.html'), status: 404
   end
