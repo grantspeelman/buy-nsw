9d312e98e521be0ee872129b258c014d7cb05bfa
Jordan Hatch <jordan@jordanh.net>
2018-03-15 11:07:39 +1100

    Force SSL if the config var is set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  force_ssl if: :ssl_configured?
   before_action :authenticate!
 
 private
@@ -9,6 +11,10 @@ private
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_USERNAME'] && password == ENV['BASIC_PASSWORD']
     end
+  end
+
+  def ssl_configured?
+    ENV['FORCE_SSL'].present?
   end
 
   def error_404
