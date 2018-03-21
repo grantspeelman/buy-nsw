16eaf08830ade6d935f0d37f4539d7a3cdd7e9cf
Jordan Hatch <jordan@jordanh.net>
2018-03-21 17:13:23 +1100

    Move form translation hackery into a shared class

--- app/forms/base_form.rb
+++ app/forms/base_form.rb
@@ -10,4 +10,20 @@ class BaseForm < Reform::Form
       end
     }.compact.any?
   end
+
+  # The following bit of hackery bodges the way that the model name is returned
+  # back from ActiveModel. It's important to set this correctly otherwise the
+  # I18n translation paths will always be the same for all forms.
+  #
+  class << self
+    def model_name
+      AdaptiveModelName.new(super)
+    end
+  end
+
+  class AdaptiveModelName < SimpleDelegator
+    def name
+      __getobj__.instance_variable_get(:@klass).name
+    end
+  end
 end
--- app/forms/sellers/applications/base_form.rb
+++ app/forms/sellers/applications/base_form.rb
@@ -72,22 +72,6 @@ class Sellers::Applications::BaseForm < BaseForm
     new_messages.any? ? new_errors : super
   end
 
-  # The following bit of hackery bodges the way that the model name is returned
-  # back from ActiveModel. It's important to set this correctly otherwise the
-  # I18n translation paths will always be the same for all forms.
-  #
-  class << self
-    def model_name
-      AdaptiveModelName.new(super)
-    end
-  end
-
-  class AdaptiveModelName < SimpleDelegator
-    def name
-      __getobj__.instance_variable_get(:@klass).name
-    end
-  end
-
   # This Populator is used by multiple forms to handle loading, saving and
   # deletion of child objects.
   #
