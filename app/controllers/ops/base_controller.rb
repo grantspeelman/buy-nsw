d16c92d73ff7968a5a8ce93778b06d53b62ec76c
Jordan Hatch <jordan@jordanh.net>
2018-03-29 14:09:41 +1100

    Make the Ops authorisation check more robust

--- app/controllers/ops/base_controller.rb
+++ app/controllers/ops/base_controller.rb
@@ -1,11 +1,11 @@
 class Ops::BaseController < ApplicationController
   before_action :authenticate_admin!
-  
+
   layout 'ops'
 
 private
   def authenticate_admin!
-    unless current_user.is_admin?
+    unless current_user && current_user.is_admin?
       raise NotAuthorized
     end
   end
