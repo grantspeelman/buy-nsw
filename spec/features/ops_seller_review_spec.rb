789d532500e7beffd5e177d268f9971e9ee90502
Jordan Hatch <jordan@jordanh.net>
2018-04-04 16:55:18 +1000

    Add feature spec for approving a seller

---
+++ spec/features/ops_seller_review_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe 'Reviewing seller applications', type: :feature, js: true do
+
+  describe 'as an admin user', admin_user: true do
+    it 'can approve an application' do
+      application = create(:awaiting_assignment_seller_application)
+
+      visit '/ops'
+      click_on 'Seller applications'
+
+      select_application_from_list(application.seller.name)
+
+      expect_application_state('awaiting_assignment')
+
+      assign_application_to(@user.email)
+
+      expect_flash_message('Application assigned')
+      expect_application_state('assigned')
+
+      decide_on_application(decision: 'Approve', response: 'Response text')
+      expect_flash_message('Application approved')
+
+      expect_application_state('approved')
+    end
+  end
+
+  def select_application_from_list(seller_name)
+    within '.record-list table' do
+      row = page.find('td', text: seller_name).ancestor('tr')
+
+      within row do
+        click_on 'View'
+      end
+    end
+  end
+
+  def assign_application_to(email)
+    page.find('h2', text: 'Assign this application').trigger('click')
+    select email, from: 'Assigned to'
+    click_on 'Assign'
+  end
+
+  def expect_application_state(state)
+    within '.status-indicator' do
+      expect(page).to have_content(state)
+    end
+  end
+
+  def expect_flash_message(message)
+    within '.messages' do
+      expect(page).to have_content(message)
+    end
+  end
+
+  def decide_on_application(decision:, response:)
+    page.find('h2', text: 'Make a decision').trigger('click')
+
+    within_fieldset 'Outcome' do
+      choose decision
+    end
+
+    fill_in 'Feedback', with: response
+
+    click_on 'Make decision'
+  end
+
+end
