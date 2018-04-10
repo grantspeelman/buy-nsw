03dbd403ff66b6367fd06efee76b6fad52aaaf18
Jordan Hatch <jordan@jordanh.net>
2018-04-10 13:45:17 +1000

    Fix BuyerApplication operation loading
    Unfortunately, separating out the concerns from Buyers::BuyerApplication::Update causes intermittent exceptions from autoloading.
    
    I've tried a few different approaches to fix this and none seem to work, so just merging back into a single file again for the time being.

--- app/concepts/buyers/buyer_application/operation/update.rb
+++ app/concepts/buyers/buyer_application/operation/update.rb
@@ -1,9 +1,78 @@
-require_relative 'update/present'
-require_relative 'update/steps'
-
 class Buyers::BuyerApplication::Update < Trailblazer::Operation
-  include Steps
+  module Steps
+    extend ActiveSupport::Concern
 
+    def steps!(options, **)
+      options['result.steps'] = step_contracts(options).map {|contract|
+        Buyers::BuyerApplication::Step.new(
+          application: options[:application_model],
+          buyer: options[:buyer_model],
+          contract_class: contract,
+        )
+      }
+    end
+
+    def step_contracts(options)
+      steps = []
+
+      steps << Buyers::BuyerApplication::Contract::BasicDetails
+
+      if options[:application_model].requires_email_approval?
+        steps << Buyers::BuyerApplication::Contract::EmailApproval
+      end
+
+      steps << Buyers::BuyerApplication::Contract::EmploymentStatus
+
+      if options[:application_model].requires_manager_approval?
+        steps << Buyers::BuyerApplication::Contract::ManagerApproval
+      end
+
+      steps << Buyers::BuyerApplication::Contract::Terms
+
+      steps
+    end
+  end
+
+  class Buyers::BuyerApplication::Update::Present < Trailblazer::Operation
+    include Steps
+
+    step :model!
+    step :steps!
+    step Contract::Build( builder: :build_contract_from_step! )
+
+    success :prevalidate_if_started!
+    success :set_submission_status!
+
+    def model!(options, params:, **)
+      options[:application_model] = BuyerApplication.created.find_by_user_and_application(options['current_user'], params[:id])
+      options[:buyer_model] = options[:application_model].buyer
+
+      options[:application_model].present?
+    end
+
+    def build_contract_from_step!(options, params:, **)
+      slug = params.fetch(:step, nil)
+      options['result.step'] =
+        options['result.steps'].find {|step| step.slug == slug } || options['result.steps'].first
+
+      options['result.step'].contract
+    end
+
+    def prevalidate_if_started!(options, params:, **)
+      contract = options['contract.default']
+
+      unless params.key?(:buyer_application)
+        contract.validate(params.fetch(:buyer_application, {})) if contract.started?
+      end
+    end
+
+    def set_submission_status!(options, **)
+      options['result.ready_for_submission'] =
+        options['result.steps'][0...-1].reject {|step| step.started? && step.valid? }.empty?
+    end
+  end
+
+  include Steps
   step Nested(Present)
 
   # NOTE: We use the Validate method here to assign values, but we don't care
