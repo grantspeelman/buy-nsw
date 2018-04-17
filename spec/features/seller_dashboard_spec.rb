a7030db2fad4e2f68ba53b8a2328df8f865732d6
Jordan Hatch <jordan@jordanh.net>
2018-04-17 10:46:33 +1000

    Add feature spec for seller dashboard

---
+++ spec/features/seller_dashboard_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Seller dashboard', type: :feature do
+
+  it 'shows a message to sellers awaiting review' do
+    seller = create(:inactive_seller_with_full_profile, owner: @user)
+    create(:assigned_seller_application, seller: seller)
+
+    visit '/'
+    click_on 'Seller dashboard'
+
+    expect(page).to have_content('Seller dashboard')
+    expect(page).to have_content('Your seller application is under review')
+  end
+
+  it 'shows a message to sellers with an active profile' do
+    seller = create(:active_seller, owner: @user)
+
+    visit '/'
+    click_on 'Seller dashboard'
+
+    expect(page).to have_content('Seller dashboard')
+    expect(page).to have_content('Your seller account is active')
+  end
+
+
+end
