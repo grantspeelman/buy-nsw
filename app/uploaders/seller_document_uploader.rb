29e8f0e2ab6855b1622f12952006fcd667ec43f6
Matthew Landauer <mlandauer@gmail.com>
2018-04-17 16:46:10 +1000

    Limit upload size. Fixes #46

--- app/uploaders/seller_document_uploader.rb
+++ app/uploaders/seller_document_uploader.rb
@@ -1,4 +1,10 @@
 class SellerDocumentUploader < CarrierWave::Uploader::Base
+  # This number should be the same as the guidance in the content:
+  # app/views/sellers/applications/_documents_form.html.erb
+  def size_range
+    1..5.megabytes
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
