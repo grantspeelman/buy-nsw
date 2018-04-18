42d026559b7a7661591e986ea390702d1b7d9221
Jordan Hatch <jordan@jordanh.net>
2018-04-18 15:42:40 +1000

    Move and update form views
    This moves the form views to correspond with their new `Contract` name, and updates the i18n keys accordingly.

--- app/helpers/forms/label_helper.rb
+++ app/helpers/forms/label_helper.rb
@@ -1,5 +1,5 @@
 module Forms::LabelHelper
   def service_description_text(key)
-    I18n.translate("activerecord.help.sellers/applications/services_form.#{key}")
+    I18n.translate("activerecord.help.sellers/seller_application/contract/services.#{key}")
   end
 end
