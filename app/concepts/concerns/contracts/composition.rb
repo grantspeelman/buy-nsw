259f7c521d744a438c10338a35166e63c9d391c6
Jordan Hatch <jordan@jordanh.net>
2018-04-18 15:51:14 +1000

    Fix error handling for nested fields
    Trailblazer operations appear to handle differently the storage of error messages on nested objects.
    
    This is mostly a good thing, as it validates the objects with less hackery than before.
    
    However (there's always a catch with TRB) – this breaks the error handling here as Reform no longer likes receiving errors as hashes.
    
    This commit changes the error handling to store a single generic error message against the field name when a nested child has an error.
    
    (This appears to fix the problem of addresses not showing a message when you don't start to add one.)

--- app/concepts/concerns/contracts/composition.rb
+++ app/concepts/concerns/contracts/composition.rb
@@ -14,11 +14,23 @@ module Concerns::Contracts::Composition
 
     new_messages.each do |model, errors|
       errors.each do |field, msgs|
-        # Support accessing the keys as both strings and symbols, as the form
-        # builder seems to require both for all fields to work.
-        #
-        new_errors.messages[field.to_s] = msgs
-        new_errors.messages[field] = msgs
+        if msgs.is_a?(Hash)
+          # Errors on nested objects (which are hashes) are handled automatically
+          # and properly through Reform, so there's no need to rewrite them here.
+          #
+          # However, setting a value in the error keys will ensure that an
+          # object can't be valid when it has invalid nested children.
+          #
+          new_errors.messages[field] ||= ['nested_field_error']
+        else
+          # Combine the error messages from all objects into the same namespace.
+          #
+          # Support accessing the keys as both strings and symbols, as the form
+          # builder seems to require both for all fields to work.
+          #
+          new_errors.messages[field.to_s] = msgs
+          new_errors.messages[field] = msgs
+        end
       end
     end
 
