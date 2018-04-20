a3814a8c597bb0fe93fabe91e110c5d96028d707
Matthew Landauer <mlandauer@gmail.com>
2018-04-20 17:27:50 +1000

    Add simple test for event message

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '#message' do
+    event = Event.new(
+      message_type: 'manager_approved',
+      message_params: {
+        name: "Manager",
+        email: "manager@example.com"
+      }
+    )
+    expect(event.message).to eq("Manager Manager (manager@example.com) approved the buyer")
+  end
 end
