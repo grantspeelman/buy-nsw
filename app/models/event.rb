b712f2580043d16e745372f9af746d9edd15572d
Matthew Landauer <mlandauer@gmail.com>
2018-04-20 13:03:34 +1000

    Add logging of event of manager approving the buyer

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   # The thing that was changed
   belongs_to :eventable, polymorphic: true
   # Who made the change
-  belongs_to :user
+  belongs_to :user, optional: true
 
   def self.submit_application!(user, application)
     create(
@@ -10,6 +10,15 @@ class Event < ApplicationRecord
       eventable: application,
       # TODO: Move text into config/locales/en.yml
       description: "Submitted application"
+    )
+  end
+
+  def self.manager_approve!(application)
+    create(
+      # The manager did something but they are not a user on the system
+      user: nil,
+      eventable: application,
+      description: "Manager #{application.manager_name} (#{application.manager_email}) approved the buyer"
     )
   end
 end
