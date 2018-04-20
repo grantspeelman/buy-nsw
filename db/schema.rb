5733bf33b787013d532b19216c8f30cfd4a3034a
Matthew Landauer <mlandauer@gmail.com>
2018-04-20 15:11:30 +1000

    For events, description and eventable need to be set

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420004019) do
+ActiveRecord::Schema.define(version: 20180420050641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +46,9 @@ ActiveRecord::Schema.define(version: 20180420004019) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.text "description"
-    t.integer "eventable_id"
-    t.string "eventable_type"
+    t.text "description", null: false
+    t.integer "eventable_id", null: false
+    t.string "eventable_type", null: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
