3b3fa9e89220dd61db790f18b48fd284157f3785
Matthew Landauer <mlandauer@gmail.com>
2018-04-20 17:17:31 +1000

    Rename methods

--- app/concepts/buyers/buyer_application/operation/update.rb
+++ app/concepts/buyers/buyer_application/operation/update.rb
@@ -104,7 +104,7 @@ class Buyers::BuyerApplication::Update < Trailblazer::Operation
 
   def log_event!(options, **)
     if options['result.submitted']
-      Event.submit_application!(
+      Event.submitted_application!(
         options['current_user'], options[:application_model]
       )
     end
