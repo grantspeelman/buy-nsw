ef259a611a574521a021bcc6f973cf10e6b36537
Matthew Landauer <mlandauer@gmail.com>
2018-05-11 11:15:13 +1000

    Extract functions

--- app/models/csv_convert.rb
+++ app/models/csv_convert.rb
@@ -20,12 +20,7 @@ module CsvConvert
           atts[s[1]] = v
         end
       end
-      # Parse industry specially
-      # Note we're using eval WHICH IS USUALLY A VERY BAD THING
-      # but in this case the csv data will never be edited by
-      # anyone in the outside world
-      atts['industry'] = eval(atts['industry'])
-      atts['services'] = eval(atts['services'])
+      atts = unstringify_seller_atts(atts)
 
       seller = Seller.find_by(id: atts['id'])
       if seller
@@ -65,21 +60,7 @@ module CsvConvert
           atts[s[1]] = v
         end
       end
-      atts['audiences'] = eval(atts['audiences'])
-      atts['pricing_variables'] = eval(atts['pricing_variables'])
-      atts['government_network_type'] = eval(atts['government_network_type'])
-      atts['supported_browsers'] = eval(atts['supported_browsers'])
-      atts['supported_os'] = eval(atts['supported_os'])
-      atts['support_options'] = eval(atts['support_options'])
-      atts['data_import_formats'] = eval(atts['data_import_formats'])
-      atts['data_export_formats'] = eval(atts['data_export_formats'])
-      atts['encryption_transit_user_types'] = eval(atts['encryption_transit_user_types'])
-      atts['encryption_transit_network_types'] = eval(atts['encryption_transit_network_types'])
-      atts['encryption_rest_types'] = eval(atts['encryption_rest_types'])
-      atts['authentication_types'] = eval(atts['authentication_types'])
-      atts['outage_channel_types'] = eval(atts['outage_channel_types'])
-      atts['metrics_channel_types'] = eval(atts['metrics_channel_types'])
-      atts['usage_channel_types'] = eval(atts['usage_channel_types'])
+      atts = unstringify_product_atts(atts)
 
       product = Product.find_by(id: atts['id'])
       if product
@@ -88,6 +69,35 @@ module CsvConvert
         Product.create!(atts)
       end
     end
+  end
+
+  def CsvConvert.unstringify_seller_atts(atts)
+    # Parse industry specially
+    # Note we're using eval WHICH IS USUALLY A VERY BAD THING
+    # but in this case the csv data will never be edited by
+    # anyone in the outside world
+    atts['industry'] = eval(atts['industry'])
+    atts['services'] = eval(atts['services'])
+    atts
+  end
+
+  def CsvConvert.unstringify_product_atts(atts)
+    atts['audiences'] = eval(atts['audiences'])
+    atts['pricing_variables'] = eval(atts['pricing_variables'])
+    atts['government_network_type'] = eval(atts['government_network_type'])
+    atts['supported_browsers'] = eval(atts['supported_browsers'])
+    atts['supported_os'] = eval(atts['supported_os'])
+    atts['support_options'] = eval(atts['support_options'])
+    atts['data_import_formats'] = eval(atts['data_import_formats'])
+    atts['data_export_formats'] = eval(atts['data_export_formats'])
+    atts['encryption_transit_user_types'] = eval(atts['encryption_transit_user_types'])
+    atts['encryption_transit_network_types'] = eval(atts['encryption_transit_network_types'])
+    atts['encryption_rest_types'] = eval(atts['encryption_rest_types'])
+    atts['authentication_types'] = eval(atts['authentication_types'])
+    atts['outage_channel_types'] = eval(atts['outage_channel_types'])
+    atts['metrics_channel_types'] = eval(atts['metrics_channel_types'])
+    atts['usage_channel_types'] = eval(atts['usage_channel_types'])
+    atts
   end
 
   def CsvConvert.export_sellers(filename)
