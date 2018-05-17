60f7b7d3bac58e55869719ec62eef32083b42e3f
Jordan Hatch <jordan@jordanh.net>
2018-05-17 11:41:27 +1000

    Remove unused path route
    This route is no longer required as the seller onboarding steps are handled by the question group routes (above).
    
    This has since been causing exceptions when erroneous requests fell back to this route. (eg /invitations)

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,6 @@ Rails.application.routes.draw do
         end
       end
     end
-    get '/applications/:id/:step', to: 'applications#show', as: :application_step
 
     resources :profiles, only: :show
     get '/search', to: 'search#search', as: :search
@@ -117,6 +116,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static#contact'
   get '/privacy', to: 'static#privacy'
   get '/terms-of-use', to: 'static#terms_of_use'
-  
+
   root to: 'static#index'
 end
