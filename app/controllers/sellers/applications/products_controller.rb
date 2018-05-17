4422f1921c91b0cf2e2c7784286eb3917237dcb7
Jordan Hatch <jordan@jordanh.net>
2018-05-17 16:45:54 +1000

    Move the product list to a separate page

--- app/controllers/sellers/applications/products_controller.rb
+++ app/controllers/sellers/applications/products_controller.rb
@@ -1,8 +1,9 @@
 class Sellers::Applications::ProductsController < Sellers::Applications::QuestionGroupController
-  layout '../sellers/applications/products/_layout'
+  layout -> {
+    action_name == 'index' ? '../sellers/applications/shared/_layout' : '../sellers/applications/products/_layout'
+  }
 
   def index
-    redirect_to sellers_application_path(params[:application_id])
   end
 
   def new
@@ -16,6 +17,20 @@ private
     @application ||= current_user.seller_applications.created.find(params[:application_id])
   end
   helper_method :application
+
+  def products
+    application.seller.products.order('name ASC')
+  end
+  helper_method :products
+
+  def progress_report
+    @progress_report ||= SellerApplicationProgressReport.new(
+      application: application,
+      question_sets: [],
+      product_question_set: operation_present_class,
+    )
+  end
+  helper_method :progress_report
 
   def operation_class
     Sellers::SellerApplication::Products::Update
