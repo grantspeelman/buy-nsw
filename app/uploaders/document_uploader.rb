898b40ff6d009e50dffd520761f5c1dd8584d95b
Jordan Hatch <jordan@jordanh.net>
2018-05-21 14:48:46 +1000

    Rename files on upload

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -16,4 +16,14 @@ class DocumentUploader < CarrierWave::Uploader::Base
   def content_type_whitelist
     ['image/jpeg', 'image/png', 'application/pdf']
   end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
