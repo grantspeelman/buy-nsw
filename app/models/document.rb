8cb08248e22677207c787b0c557b4d93c5fa39c1
Jordan Hatch <jordan@jordanh.net>
2018-05-21 12:43:57 +1000

    Scan documents with ClamAV on a queue

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,17 +2,24 @@ class Document < ApplicationRecord
   extend Enumerize
 
   belongs_to :documentable, polymorphic: true
-  before_save :update_document_attributes
   validates :kind, :scan_status, presence: true
   mount_uploader :document, DocumentUploader
-  
+
+  before_save :update_document_attributes
+  after_save :scan_file
+
   enumerize :scan_status, in: ['unscanned', 'clean', 'infected']
 
   def update_document_attributes
     if document.present? && document_changed?
+      self.scan_status = 'unscanned'
       self.original_filename = document.file.original_filename
       self.content_type = document.file.content_type
     end
+  end
+
+  def scan_file
+    DocumentScanJob.perform_later(self) if saved_change_to_attribute?(:document)
   end
 
 end
---
+++ spec/jobs/document_scan_job_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+require 'carrierwave/storage/fog'
+
+RSpec.describe DocumentScanJob, type: :job do
+
+  let(:document) { create(:unscanned_document) }
+  let(:file_path) { document.document.file.path }
+  let(:example_file_body) {
+    File.read(Rails.root.join('spec','fixtures','files','example.pdf'))
+  }
+
+  describe '#perform' do
+    it 'sets the status of a clean file' do
+      expect(Clamby).to receive(:safe?).with(file_path).and_return(true)
+      described_class.perform_now(document)
+
+      expect(document.reload.scan_status).to eq('clean')
+    end
+
+    it 'sets the status of an infected file' do
+      expect(Clamby).to receive(:safe?).with(file_path).and_return(false)
+      described_class.perform_now(document)
+
+      expect(document.reload.scan_status).to eq('infected')
+    end
+
+    describe 'for a remote file (eg. S3)' do
+      it 'downloads a remote file to the tmp folder' do
+        tmp_file_path = Rails.root.join('tmp', 'scan', document.to_param, 'example.pdf')
+        url = "http://example.org/uploads/document/#{document.to_param}/example.pdf"
+
+        expect(CarrierWave::Uploader::Base).to receive(:storage).and_return(CarrierWave::Storage::Fog)
+        expect(document.document).to receive(:url).and_return(url)
+        expect_any_instance_of(described_class).to receive(:open).with(url).and_return(example_file_body)
+        expect(Clamby).to receive(:safe?).with(tmp_file_path).and_return(true)
+
+        described_class.perform_now(document)
+
+        expect(document.reload.scan_status).to eq('clean')
+      end
+    end
+  end
+
+end
