5ee96c8ec896980d39dfd994e8c59566ba963755
Jordan Hatch <jordan@jordanh.net>
2018-05-22 15:11:23 +1000

    Refactor the ABN factory helper

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,22 @@
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
+
+module FactoryBot
+  module Syntax
+    module Methods
+      def build_abn(n)
+        main = n.to_s.rjust(9, "0")
+        weights = [3, 5, 7, 9, 11, 13, 15, 17, 19]
+        sum = 0
+        (0..8).each do |i|
+          digit = main[i,1].to_i
+          sum += weights[i] * digit
+        end
+        checksum = 99 - sum % 89
+        checksum_string = checksum.to_s.rjust(2, "0")
+        checksum_string + main
+      end
+    end
+  end
+end
