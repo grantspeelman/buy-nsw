9b329c61a94e3d3323e2e387e4fce7069e6e0838
Jordan Hatch <jordan@jordanh.net>
2018-05-25 12:37:34 +1000

    Consolidate seller application controllers

--- app/controllers/sellers/applications/root_controller.rb
+++ app/controllers/sellers/applications/root_controller.rb
@@ -1,4 +1,6 @@
 class Sellers::Applications::RootController < Sellers::Applications::BaseController
+  before_action :assert_application_presence!, except: :new
+
   def new
     run Sellers::SellerApplication::Create do |result|
       return redirect_to sellers_application_path(result[:application_model].id)
@@ -7,58 +9,41 @@ class Sellers::Applications::RootController < Sellers::Applications::BaseControl
     redirect_to sellers_dashboard_path
   end
 
-  def show
-    unless tailor_step_valid?
-      return redirect_to(tailor_sellers_application_path(application))
-    end
-
-    render :show
+  def submit
+    @operation = run Sellers::SellerApplication::Submit::Present
   end
 
-  def submit
-    if ! can_submit_application?
-      return redirect_to sellers_application_path(application)
-    end
-
-    run Sellers::SellerApplication::Submit do |result|
+  def do_submit
+    @operation = run Sellers::SellerApplication::Submit do |result|
       return redirect_to sellers_dashboard_path
     end
 
-    return redirect_to sellers_application_path(application)
+    render :submit
   end
 
 private
-  def question_sets
-    [
-      Sellers::SellerApplication::Tailor::Update::Present,
-      Sellers::SellerApplication::Contacts::Update::Present,
-      Sellers::SellerApplication::Documents::Update::Present,
-      Sellers::SellerApplication::Profile::Update::Present,
-      Sellers::SellerApplication::Disclosures::Update::Present,
-      Sellers::SellerApplication::Legals::Update::Present,
-    ]
-  end
+  attr_reader :operation
+  helper_method :operation
 
-  def progress_report
-    @progress_report ||= SellerApplicationProgressReport.new(
-      application: application,
-      question_sets: question_sets,
-      product_question_set: Sellers::SellerApplication::Products::Update::Present,
+  def steps
+    Sellers::Applications::StepsController.steps
+  end
+  helper_method :steps
+
+  def presenter
+    @presenter ||= Sellers::Applications::DashboardPresenter.new(
+      application,
+      steps,
     )
   end
-  helper_method :progress_report
-
-  def tailor_step_valid?
-    application.tailor_complete?
-  end
-
-  def can_submit_application?
-    progress_report.all_question_sets_valid?
-  end
-  helper_method :can_submit_application?
+  helper_method :presenter
 
   def submit_form
     @operation ||= run(Sellers::SellerApplication::Submit::Present)
   end
   helper_method :submit_form
+
+  def assert_application_presence!
+    raise NotFound unless application.present?
+  end
 end
