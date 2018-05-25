a28bf7b261f625c65dcc84aae1cec49e024e6271
Jordan Hatch <jordan@jordanh.net>
2018-05-25 15:20:34 +1000

    Rework the initial step to check for cloud products

--- app/concepts/sellers/seller_application/contract/services.rb
+++ app/concepts/sellers/seller_application/contract/services.rb
@@ -1,10 +1,39 @@
 module Sellers::SellerApplication::Contract
   class Services < Base
+    property :offers_cloud, virtual: true
     property :services, on: :seller
 
+    def offers_cloud
+      self.services.include?('cloud-services')
+    end
+
+    def deserialize!(document)
+      hash = super(document)
+
+      if hash[:services]
+        if hash[:offers_cloud] == 'true'
+          hash[:services] << 'cloud-services'
+        else
+          services.delete('cloud-services')
+        end
+      end
+
+      hash
+    end
+
     validation :default, inherit: true do
+      configure do
+        def offers_cloud?(list)
+          list.include?('cloud-services')
+        end
+      end
+
       required(:seller).schema do
         required(:services).value(any_checked?: true, one_of?: Seller.services.values)
+
+        rule(offers_cloud: [:services]) do |services|
+          services.offers_cloud?
+        end
       end
     end
   end
