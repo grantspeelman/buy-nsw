adf63c988a8a4b60c40b956ec64a4c71ebe6a534
Jordan Hatch <jordan@jordanh.net>
2018-05-25 12:46:04 +1000

    Update the onboarding feature spec for the new journey

--- spec/features/seller_onboarding_spec.rb
+++ spec/features/seller_onboarding_spec.rb
@@ -8,15 +8,23 @@ RSpec.describe 'Seller onboarding', type: :feature, js: true, skip_login: true d
     complete_seller_sign_up
     confirm_email_address
 
-    complete_tailor_steps
-    complete_contacts_steps
-    complete_profile_steps
-    complete_documents_steps
-    complete_disclosures_steps
+    complete_services
+    complete_business_details
+    complete_contacts
+    complete_addresses
+    complete_characteristics
+    complete_disclosures
+    complete_profile_basics
+    complete_recognition
+
+    complete_financial_statement
+    complete_product_liability
+    complete_professional_indemnity
+    complete_workers_compensation
 
     invite_team_member
     accept_invitation
-    complete_legals_steps
+    complete_declaration
 
     submit_application
 
@@ -42,86 +50,153 @@ RSpec.describe 'Seller onboarding', type: :feature, js: true, skip_login: true d
     expect(page).to have_content('Thanks for confirming')
   end
 
-  def complete_tailor_steps
-    click_on 'Start application'
-
-    # Business details
-    fill_in 'Business name', with: 'OpenAustralia Foundation Ltd'
-    fill_in 'ABN', with: '24 138 089 942'
-    click_on 'Save'
+  def complete_services
+    click_on_step 'services'
 
     # Services
     check 'Cloud products'
     check 'Training and learning'
     click_on 'Save'
-
-    # Review
-    click_on 'Continue'
   end
 
-  def complete_contacts_steps
-    within '#contacts' do
-      click_on 'Start'
-    end
+  def complete_business_details
+    click_on_step 'business_details'
 
-    fill_in_contact_details
-
-    click_on_step 'Business address'
-    fill_in_address
+    # Business details
+    fill_in 'Business name', with: 'OpenAustralia Foundation Ltd'
+    fill_in 'ABN', with: '24 138 089 942'
+    click_on 'Save'
   end
 
-  def complete_profile_steps
-    within '#profile' do
-      click_on 'Start'
+  def complete_contacts
+    click_on_step 'contacts'
+
+    within_fieldset 'Business contact' do
+      fill_in 'Name', with: 'Winston Smith-Churchill'
+      fill_in 'Email address', with: 'test@example.org'
+      fill_in 'Phone', with: '0412 345 678'
     end
+    within_fieldset 'Authorised representative' do
+      fill_in 'Name', with: 'Churchill Smith-Winston'
+      fill_in 'Email address', with: 'authorised@test.org'
+      fill_in 'Phone', with: '0487 654 321'
+      fill_in 'Position', with: 'CEO'
+    end
+    click_on 'Save'
+  end
+
+  def complete_addresses
+    click_on_step 'addresses'
+
+    fill_in 'Address', with: '123 Test Street'
+    fill_in 'Suburb', with: 'Millers Point'
+    select 'New South Wales', from: 'State'
+    fill_in 'Postcode', with: '2000'
+
+    click_on 'Add another address'
+
+    within 'ol li:last-of-type' do
+      fill_in 'Address', with: '321 Test Street'
+      fill_in 'Suburb', with: 'Millers Point'
+      select 'New South Wales', from: 'State'
+      fill_in 'Postcode', with: '2000'
+    end
+
+    click_on 'Save'
+  end
+
+  def complete_characteristics
+    click_on_step 'characteristics'
+
+    choose '2-4'
+    choose 'Standalone organisation'
+    check 'Start-up'
+    check 'Not for profit'
+    check 'Regional'
+    check 'Aboriginal'
+    check 'Yes, with local government'
+    check 'Yes, with federal government'
+    click_on 'Save'
+  end
+
+  def complete_disclosures
+    click_on_step 'disclosures'
+
+    fill_in_disclosure :receivership, 'No'
+    fill_in_disclosure :investigations, 'Yes', 'Further details'
+    fill_in_disclosure :legal_proceedings, 'No'
+    fill_in_disclosure :insurance_claims, 'Yes', 'Further details'
+    fill_in_disclosure :conflicts_of_interest, 'Yes', 'Further details'
+    fill_in_disclosure :other_circumstances, 'No'
+    click_on 'Save'
+  end
+
+  def complete_profile_basics
+    click_on_step 'profile_basics'
 
     fill_in 'Summary', with: 'A summary of my business'
     fill_in 'Website URL', with: 'http://example.org'
     fill_in 'LinkedIn URL', with: 'http://linkedin.com/example'
     click_on 'Save'
-
-    click_on_step 'Business details'
-    fill_in_business_details
-
-    click_on_step 'Recognition'
-    fill_in_recognition
   end
 
-  def complete_documents_steps
-    within '#documents' do
-      click_on 'Start'
+  def complete_recognition
+    click_on_step 'recognition'
+
+    within_fieldset 'Accreditations' do
+      fill_in '1', with: 'Certified Pie Eater'
+      fill_in '2', with: 'Accredited Cheese Taster'
+      click_on 'Add another row'
+      fill_in '3', with: 'Verified Beer Drinker'
     end
 
-    upload_document
+    within_fieldset 'Industry engagement' do
+      fill_in '1', with: 'Wigan Bakers Association board member'
+    end
 
-    click_on_step 'Professional indemnity'
-    upload_document
+    within_fieldset 'Awards' do
+      fill_in '1', with: 'Butter Pie of the Year 2018'
+    end
 
-    click_on_step 'Workers compensation'
-    upload_document(button_label: 'Save')
-
-    click_on_step 'Product liability'
     click_on 'Save'
   end
 
-  def complete_disclosures_steps
-    within '#disclosures' do
-      click_on 'Start'
-    end
-
-    fill_in_disclosures
+  def complete_financial_statement
+    click_on_step 'financial_statement'
+    upload_document
   end
 
-  def complete_legals_steps
-    within '#legals' do
-      click_on 'Start'
-    end
+  def complete_product_liability
+    click_on_step 'product_liability'
+    upload_document
+  end
 
-    complete_declaration
+  def complete_professional_indemnity
+    click_on_step 'professional_indemnity'
+    upload_document
+  end
+
+  def complete_workers_compensation
+    click_on_step 'workers_compensation'
+    check form_label('workers_compensation.workers_compensation_exempt')
+    click_on 'Save'
+  end
+
+  def complete_declaration
+    click_on_step 'declaration'
+
+    check 'I am Churchill Smith-Winston, an authorised representative of OpenAustralia Foundation Ltd (ABN: 24 138 089 942)'
+    click_on 'Save'
+  end
+
+  def submit_application
+    click_on_step 'submit'
+    click_on 'Submit application'
   end
 
   def invite_team_member
-    click_on 'Invite'
+    click_on 'Invite your team'
+    click_on 'Invite a team member'
 
     fill_in 'Email', with: 'authorised@test.org'
     click_on 'Send invitation'
@@ -139,62 +214,6 @@ RSpec.describe 'Seller onboarding', type: :feature, js: true, skip_login: true d
     click_on 'Accept'
 
     expect(page).to have_content('accepted')
-  end
-
-  def fill_in_address
-    within_fieldset 'Your business address' do
-      fill_in 'Address', with: '123 Test Street'
-      fill_in 'Suburb', with: 'Millers Point'
-      select 'New South Wales', from: 'State'
-      fill_in 'Postcode', with: '2000'
-
-      click_on 'Add another address'
-
-      within 'ol li:last-of-type' do
-        fill_in 'Address', with: '321 Test Street'
-        fill_in 'Suburb', with: 'Millers Point'
-        select 'New South Wales', from: 'State'
-        fill_in 'Postcode', with: '2000'
-      end
-    end
-    click_on 'Save'
-  end
-
-  def fill_in_business_details
-    choose '2-4'
-    choose 'Standalone organisation'
-    check 'Start-up'
-    check 'Not for profit'
-    check 'Regional'
-    check 'Aboriginal'
-    check 'Yes, with local government'
-    check 'Yes, with federal government'
-    click_on 'Save'
-  end
-
-  def fill_in_contact_details
-    within_fieldset 'Business contact' do
-      fill_in 'Name', with: 'Winston Smith-Churchill'
-      fill_in 'Email address', with: 'test@example.org'
-      fill_in 'Phone', with: '0412 345 678'
-    end
-    within_fieldset 'Authorised representative' do
-      fill_in 'Name', with: 'Churchill Smith-Winston'
-      fill_in 'Email address', with: 'authorised@test.org'
-      fill_in 'Phone', with: '0487 654 321'
-      fill_in 'Position', with: 'CEO'
-    end
-    click_on 'Save'
-  end
-
-  def fill_in_disclosures
-    fill_in_disclosure :receivership, 'No'
-    fill_in_disclosure :investigations, 'Yes', 'Further details'
-    fill_in_disclosure :legal_proceedings, 'No'
-    fill_in_disclosure :insurance_claims, 'Yes', 'Further details'
-    fill_in_disclosure :conflicts_of_interest, 'Yes', 'Further details'
-    fill_in_disclosure :other_circumstances, 'No'
-    click_on 'Save'
   end
 
   def fill_in_disclosure(field, option, details=nil)
@@ -224,39 +243,18 @@ RSpec.describe 'Seller onboarding', type: :feature, js: true, skip_login: true d
     Rails.root.join('spec', 'fixtures', 'files', 'example.pdf')
   end
 
-  def fill_in_recognition
-    within_fieldset 'Accreditations' do
-      fill_in '1', with: 'Certified Pie Eater'
-      fill_in '2', with: 'Accredited Cheese Taster'
-      click_on 'Add another row'
-      fill_in '3', with: 'Verified Beer Drinker'
+  def click_on_step(key)
+    within '.task-list' do
+      click_link(step_label(key))
     end
-
-    within_fieldset 'Industry engagement' do
-      fill_in '1', with: 'Wigan Bakers Association board member'
-    end
-
-    within_fieldset 'Awards' do
-      fill_in '1', with: 'Butter Pie of the Year 2018'
-    end
-
-    click_on 'Save'
   end
 
-  def complete_declaration
-    check 'I am Churchill Smith-Winston, an authorised representative of OpenAustralia Foundation Ltd (ABN: 24 138 089 942)'
-    click_on 'Save'
+  def step_label(key)
+    I18n.t("sellers.applications.steps.#{key}.short")
   end
 
-  def submit_application
-    click_on 'Submit application'
-  end
-
-  def click_on_step(label)
-    within '.steps-list' do
-      node = page.find('a', text: label)
-      node.click
-    end
+  def form_label(key)
+    I18n.t("activemodel.attributes.sellers.seller_application.contract.#{key}")
   end
 
 end
