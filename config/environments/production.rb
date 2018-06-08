e0bd35011a1a6b5a7b7f5117fcc070062c33b2f4
Matthew Landauer <mlandauer@gmail.com>
2018-06-08 11:24:37 +1000

    Add feature flag so that we can disable rate limiting in staging for some testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.middleware.use Rack::Attack
-
+  unless ENV['FEATURE_FLAG_DISABLE_RATE_LIMITING'].present?
+    config.middleware.use Rack::Attack
+  end
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
