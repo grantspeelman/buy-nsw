9139d729ad7f11ec647d31c9b7531947e3c8e359
Jordan Hatch <jordan@jordanh.net>
2018-06-12 11:46:46 +1000

    Browse the ops product pages in seller review spec

--- spec/features/ops_seller_review_spec.rb
+++ spec/features/ops_seller_review_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Reviewing seller applications', type: :feature, js: true do
   describe 'as an admin user', user: :admin_user do
     it 'can approve an application' do
       application = create(:awaiting_assignment_seller_application)
+      product = create(:inactive_product, :with_basic_details, seller: application.seller)
 
       visit '/ops'
       click_on 'Seller applications'
@@ -20,6 +21,7 @@ RSpec.describe 'Reviewing seller applications', type: :feature, js: true do
       expect_application_state('ready_for_review')
 
       browse_application_details
+      browse_product(product)
 
       decide_on_application(decision: 'Approve', response: 'Response text')
       expect_flash_message('Application approved')
@@ -60,6 +62,13 @@ RSpec.describe 'Reviewing seller applications', type: :feature, js: true do
     within '.right-col nav' do
       click_on 'Seller details'
       click_on 'Documents'
+      click_on 'Application'
+    end
+  end
+
+  def browse_product(product)
+    within '.right-col nav' do
+      click_on product.name
       click_on 'Application'
     end
   end
