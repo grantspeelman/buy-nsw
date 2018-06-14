bd8546f38a94be298a4ab78a3aa35e2cd98783fe
Jordan Hatch <jordan@jordanh.net>
2018-06-14 11:37:31 +1000

    Patch reform to support the full_messages_for method

--- config/initializers/reform.rb
+++ config/initializers/reform.rb
@@ -1,7 +1,15 @@
+require 'reform'
+
 # Set Reform to use dry-validation instead of ActiveModel validations
 #
 Rails.application.config.reform.validations = :dry
 
 Dry::Validation::Schema::Form.configure do |config|
   config.messages = :i18n
+end
+
+class Reform::Contract::Errors
+  def full_messages_for(key)
+    messages[key]
+  end
 end
