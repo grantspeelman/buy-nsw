0fa1129e6c5f7cfd556cb71c63f7e2799512a46f
Jordan Hatch <jordan@jordanh.net>
2018-06-26 11:24:24 +1000

    Show invitation error messages as a block

--- spec/concepts/sellers/seller_application/invitation/operation/create_spec.rb
+++ spec/concepts/sellers/seller_application/invitation/operation/create_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Sellers::SellerApplication::Invitation::Create do
 
-  subject { Sellers::SellerApplication::Invitation::Create }
-
   let(:application) { create(:created_seller_application) }
   let(:current_user) { create(:seller_user, seller: application.seller) }
 
   it 'creates a new user' do
-    result = subject.({ application_id: application.id, invitation: { email: 'blah@example.org' } }, 'config.current_user' => current_user)
+    result = described_class.({ application_id: application.id, invitation: { email: 'blah@example.org' } }, 'config.current_user' => current_user)
 
     new_user = User.find_by_email('blah@example.org')
 
@@ -21,6 +19,15 @@ RSpec.describe Sellers::SellerApplication::Invitation::Create do
 
     expect(new_user.confirmation_token).to be_present
     expect(new_user.confirmed_at).to be_blank
+  end
+
+  context 'with errors on the User model' do
+    it 'sets them on the contract' do
+      result = described_class.({ application_id: application.id, invitation: { email: current_user.email } }, 'config.current_user' => current_user)
+
+      expect(result).to be_failure
+      expect(result['contract.default'].errors[:email]).to be_present
+    end
   end
 
 end
