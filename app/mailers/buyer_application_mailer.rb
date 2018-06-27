effb6d66ba9b4cc92d3247c2cfcb8804af89e7e4
Matthew Landauer <mlandauer@gmail.com>
2018-06-27 13:16:58 +1000

    Update buyer approval email

--- app/mailers/buyer_application_mailer.rb
+++ app/mailers/buyer_application_mailer.rb
@@ -14,7 +14,7 @@ class BuyerApplicationMailer < ApplicationMailer
 
     mail(
       to: @application.user.email,
-      subject: "buy.nsw: Your application has been approved",
+      subject: "Welcome to the buy.nsw community",
     )
   end
 
