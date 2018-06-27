b98e3affe4146e42d27df3d9d7eb714a11fcabb8
Matthew Landauer <mlandauer@gmail.com>
2018-06-27 12:48:48 +1000

    Use factory bot for mailer preview

--- spec/mailers/previews/buyer_application_mailer_preview.rb
+++ spec/mailers/previews/buyer_application_mailer_preview.rb
@@ -1,19 +1,22 @@
 class BuyerApplicationMailerPreview < ActionMailer::Preview
   def manager_approval_email
-    BuyerApplicationMailer.
-      with(application: BuyerApplication.awaiting_manager_approval.first).
-      manager_approval_email
+    application = FactoryBot.create(:awaiting_manager_approval_buyer_application)
+    mail = BuyerApplicationMailer.with(application: application).manager_approval_email
+    application.destroy
+    mail
   end
 
   def application_approved_email
-    # TODO: Really should have some seed data that has an approved application already there
-    application = BuyerApplication.approved.first
-    BuyerApplicationMailer.with(application: application).application_approved_email
+    application = FactoryBot.create(:approved_buyer_application)
+    mail = BuyerApplicationMailer.with(application: application).application_approved_email
+    application.destroy
+    mail
   end
 
   def application_rejected_email
-    # TODO: Really should have some seed data that has a rejected application already there
-    application = BuyerApplication.rejected.first
-    BuyerApplicationMailer.with(application: application).application_rejected_email
+    application = FactoryBot.create(:rejected_buyer_application)
+    mail = BuyerApplicationMailer.with(application: application).application_rejected_email
+    application.destroy
+    mail
   end
 end
