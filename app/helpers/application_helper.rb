8f1587bc7c3762efa664f2f8f44b41629da57fbc
Matthew Landauer <mlandauer@gmail.com>
2018-06-28 13:29:44 +1000

    Move helper so it's available everywhere

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,4 +35,12 @@ module ApplicationHelper
   def tel_to(phone)
     link_to phone, "tel:#{phone}"
   end
+
+  # NOTE: This approach allows us to still convert new lines into paragraphs
+  # and line break tags, but escapes all HTML so that links and other formatting
+  # isn't parsed by simple_format
+  #
+  def format_text_block(text)
+    simple_format h(text)
+  end
 end
