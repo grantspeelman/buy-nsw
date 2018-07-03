3c098918167c12996bedb568a4d6a24a4e087859
Jordan Hatch <jordan@jordanh.net>
2018-07-03 11:59:48 +1000

    Add methods to ProductDecorator to handle currency

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -20,12 +20,42 @@ class ProductDecorator < BaseDecorator
     terms.document.present? && terms.scan_status == 'clean'
   end
 
+  def pricing_currency
+    super == 'other' ? pricing_currency_other : super
+  end
+
+  def pricing_min_with_currency
+    parse_money(pricing_min)
+  end
+
+  def pricing_max_with_currency
+    parse_money(pricing_max)
+  end
+
+  def formatted_pricing_min
+    pricing_min_with_currency&.format(with_currency: true)
+  end
+
+  def formatted_pricing_max
+    pricing_max_with_currency&.format(with_currency: true)
+  end
+
   def details
     ProductDetails.new(__getobj__).details
   end
 
   def all_details
     ProductDetails.new(__getobj__, include_all: true).details
+  end
+
+private
+  def parse_money(amount)
+    return nil if pricing_currency.blank?
+    begin
+      Money.new(amount, pricing_currency)
+    rescue Money::Currency::UnknownCurrency
+      nil
+    end
   end
 
 end
