f2a91b301c25967330cb32167fa9cb2e2e5673de
Jordan Hatch <jordan@jordanh.net>
2018-07-05 12:13:16 +1000

    Update Ops::SellerApplication operations to use renamed model

--- app/concepts/ops/seller_application/operation/decide.rb
+++ app/concepts/ops/seller_application/operation/decide.rb
@@ -1,6 +1,6 @@
 class Ops::SellerApplication::Decide < Trailblazer::Operation
   class Present < Trailblazer::Operation
-    step Model( SellerApplication, :find_by )
+    step Model( SellerVersion, :find_by )
     step Contract::Build( constant: Ops::SellerApplication::Contract::Decide )
   end
 
