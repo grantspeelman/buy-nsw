46ba712c05b6680d66e218adb565b4da7564fb1c
Matthew Landauer <mlandauer@gmail.com>
2018-07-05 17:03:19 +1000

    Extract method

--- app/models/slack_message.rb
+++ app/models/slack_message.rb
@@ -2,49 +2,45 @@ class SlackMessage
   include Rails.application.routes.url_helpers
 
   def new_product_order(order)
-    buyer = link_to(order.buyer.name, ops_buyer_url(order.buyer))
-    product = link_to(order.product.name, pathway_product_url(order.product.section, order.product))
-    url = ops_product_orders_url
-
-    message(
-      text: I18n.t(
-        'slack_messages.new_product_order.text',
-        buyer: buyer,
+    message_type_with_button(
+      :new_product_order,
+      {
+        buyer: link_to(order.buyer.name, ops_buyer_url(order.buyer)),
         organisation: order.buyer.organisation,
-        product: product,
-        url: url
-      ),
-      attachments: [
-        {
-          fallback: "#{I18n.t('slack_messages.new_product_order.button')} at #{url}",
-          actions: [
-            type: "button",
-            text: I18n.t('slack_messages.new_product_order.button'),
-            url: url
-          ]
-        }
-      ]
+        product: link_to(order.product.name, pathway_product_url(order.product.section, order.product))
+      },
+      ops_product_orders_url
     )
   end
 
   def buyer_application_submitted(application)
-    url = ops_buyer_application_url(application)
-    buyer = application.buyer.name
-    organisation = application.buyer.organisation
-    button = I18n.t('slack_messages.buyer_application_submitted.button')
+    message_type_with_button(
+      :buyer_application_submitted,
+      {
+        buyer: application.buyer.name,
+        organisation: application.buyer.organisation
+      },
+      ops_buyer_application_url(application)
+    )
+  end
 
+  def message_type_with_button(type, params, button_url)
+    message_with_button(
+      I18n.t("slack_messages.#{type}.text", params),
+      I18n.t("slack_messages.#{type}.button"),
+      button_url
+    )
+  end
+
+  def message_with_button(text, button_text, button_url)
     message(
-      text: I18n.t(
-        'slack_messages.buyer_application_submitted.text',
-        buyer: buyer,
-        organisation: organisation
-      ),
+      text: text,
       attachments: [{
-        fallback: "#{button} at #{url}",
+        fallback: "#{button_text} at #{button_url}",
         actions: [
           type: "button",
-          text: button,
-          url: url
+          text: button_text,
+          url: button_url
         ]
       }]
     )
