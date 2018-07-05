b40a0f5ec46aca97a2dd0f8a3a3292601b11fc2f
Matthew Landauer <mlandauer@gmail.com>
2018-07-05 12:39:57 +1000

    show for product orders isn't implemented yet. So remove it

--- app/models/slack_message.rb
+++ app/models/slack_message.rb
@@ -2,7 +2,7 @@ module SlackMessage
   def self.new_product_order(order)
     buyer = link_to(order.buyer.name, Rails.application.routes.url_helpers.ops_buyer_url(order.buyer))
     product = link_to(order.product.name, Rails.application.routes.url_helpers.pathway_product_url(order.product.section, order.product))
-    view = link_to("View product order", Rails.application.routes.url_helpers.ops_product_order_url(order))
+    view = link_to("View product order", Rails.application.routes.url_helpers.ops_product_orders_url)
     message("#{buyer} from #{order.buyer.organisation} wants to buy #{product}. #{view}.")
   end
 
