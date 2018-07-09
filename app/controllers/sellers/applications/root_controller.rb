bf7162b8e530cd531a2c936abba7fc0c9ca18845
Jordan Hatch <jordan@jordanh.net>
2018-07-09 15:47:41 +1000

    Update controller redirects on application creation

--- app/controllers/sellers/applications/root_controller.rb
+++ app/controllers/sellers/applications/root_controller.rb
@@ -6,13 +6,13 @@ class Sellers::Applications::RootController < Sellers::Applications::BaseControl
 
     if operation.failure?
       return redirect_to sellers_dashboard_path if operation['application_submitted']
-      return redirect_to sellers_application_path(operation['model.application'].id) if operation['application_created']
+      return redirect_to sellers_application_path(operation['model.seller_version'].id) if operation['application_created']
     end
   end
 
   def create
     run Sellers::SellerVersion::Create do |result|
-      return redirect_to sellers_application_path(result['model.application'].id)
+      return redirect_to sellers_application_path(result['model.seller_version'].id)
     end
 
     redirect_to new_sellers_application_path
