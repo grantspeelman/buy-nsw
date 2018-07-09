225a2eb7c5a6fd181b531542b76797f40d204fd9
Jordan Hatch <jordan@jordanh.net>
2018-07-09 14:57:17 +1000

    Update Sellers::SellerVersion::Create

--- spec/concepts/sellers/seller_version/operation/create_spec.rb
+++ spec/concepts/sellers/seller_version/operation/create_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Sellers::SellerVersion::Create do
     described_class.({ }, 'config.current_user' => user)
   end
 
-  it 'creates a seller and seller application' do
+  it 'creates a seller and seller version' do
     result = perform_operation
 
     expect(result).to be_success
@@ -16,7 +16,7 @@ RSpec.describe Sellers::SellerVersion::Create do
     expect(Seller.count).to eq(1)
     expect(SellerVersion.count).to eq(1)
 
-    expect(result['model.application']).to be_persisted
+    expect(result['model.seller_version']).to be_persisted
     expect(result['model.seller']).to be_persisted
 
     expect(result['model.seller'].owners).to contain_exactly(user)
@@ -31,7 +31,7 @@ RSpec.describe Sellers::SellerVersion::Create do
     expect(SellerVersion.count).to eq(1)
   end
 
-  it 'does not create an additional application when one exists' do
+  it 'does not create an additional version when one exists' do
     seller = create(:seller, owner: user)
     application = create(:seller_version, seller: seller)
 
@@ -41,30 +41,30 @@ RSpec.describe Sellers::SellerVersion::Create do
     expect(SellerVersion.count).to eq(1)
   end
 
-  it 'sets the started_at timestamp for a new application' do
+  it 'sets the started_at timestamp for a new version' do
     time = 1.hour.ago
 
     Timecop.freeze(time) do
       result = perform_operation
-      expect(result['model.application'].started_at.to_i).to eq(time.to_i)
+      expect(result['model.seller_version'].started_at.to_i).to eq(time.to_i)
     end
   end
 
-  it 'logs an event when the application is started' do
+  it 'logs an event when the version is started' do
     result = perform_operation
-    expect(result['model.application'].events.last.message).to eq("Started application")
-    expect(result['model.application'].events.last.user).to eq(user)
+    expect(result['model.seller_version'].events.last.message).to eq("Started application")
+    expect(result['model.seller_version'].events.last.user).to eq(user)
   end
 
-  it 'does not update the started_at timestamp for an existing application' do
+  it 'does not update the started_at timestamp for an existing version' do
     seller = create(:seller, owner: user)
     application = create(:seller_version, seller: seller, started_at: 1.hour.ago)
 
     result = perform_operation
-    expect(result['model.application'].started_at.to_i).to eq(application.started_at.to_i)
+    expect(result['model.seller_version'].started_at.to_i).to eq(application.started_at.to_i)
   end
 
-  it 'fails when an application has already been submitted' do
+  it 'fails when an version has already been submitted' do
     seller = create(:seller, owner: user)
     application = create(:awaiting_assignment_seller_version, seller: seller)
 
